0c042dc0d49f3957a46aa6fb24e5e8280729fab0
Roy Wang <falin.wang@outlook.com>
2020-08-31 01:38:32 -0400

    Finish React

--- src/Results.js
+++ src/Results.js
@@ -2,28 +2,27 @@ import React, { useState, useEffect } from "react";
 import "./Results.css";
 import VideoCard from "./VideoCard";
 import axios from "./axios";
-import requests from "./requests";
+import FlipMove from "react-flip-move";
 
-function Results() {
+function Results({ selectedOption }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const req = await axios
-        .get(requests.fetchActionMovies)
-        .catch((err) => console.log(err));
-      console.log(req);
+      const req = await axios.get(selectedOption);
       setMovies(req.data.results);
       return req;
     }
     fetchData();
-  }, []);
+  }, [selectedOption]);
 
   return (
     <div className="results">
-      {movies.map((movie) => (
-        <VideoCard movie={movie} />
-      ))}
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./VideoCard.css";
 import TextTruncate from "react-text-truncate";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function VideoCard({ movie }) {
+const VideoCard = forwardRef(({ movie }, ref) => {
   return (
-    <div className="videoCard">
+    <div ref={ref} className="videoCard">
       <img
         src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
         alt="Movie thumbnail"
@@ -19,13 +19,13 @@ function VideoCard({ movie }) {
         text={movie.overview}
       />
       <h2>{movie.title || movie.name || movie.original_name}</h2>
-      {/* <h4 className="videoCard__stats">
+      <h4 className="videoCard__stats">
         {movie.original_title && `${movie.original_title}`}
         {movie.original_name && `${movie.original_name}`}
         {<br></br>}
-      </h4> */}
+      </h4>
       <p className="videoCard__stats">
-        {movie.media_type && `${movie.media_type} ·`}
+        {movie.media_type && `${movie.media_type} · `}
         {movie.release_date || movie.first_air_date}
         {` `}
         <ThumbUpIcon />
@@ -34,6 +34,6 @@ function VideoCard({ movie }) {
       </p>
     </div>
   );
-}
+});
 
 export default VideoCard;
